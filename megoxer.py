82795543281886887fea1432bc9c14e215ee0573
jsisjss <johnlibert789@gmail.com>
2025-03-20 12:35:06 +0530

    Update megoxer.py

--- megoxer.py
+++ megoxer.py
@@ -1,7 +1,22 @@
+import os
 import base64
 import json
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
+
+def install_requirements():
+    os.system("sudo apt update")
+    os.system("sudo apt install -y ufw python3-pip")
+    os.system("sudo ufw enable")
+    os.system("sudo ufw allow ssh")
+    os.system("sudo ufw allow http")
+    os.system("sudo ufw allow https")
+    os.system("sudo ufw allow 10000:30000/udp")
+    os.system("sudo ufw allow 10000:30000/tcp")
+    os.system("sudo ufw status")
+    os.system("pip install telebot pycryptodome")
+
+install_requirements()
 
 KEY = base64.b64decode("9ghy/QYLOvNz37MlR6uvaRGV7RU0oPTq5w1zZlyZXSc=")
 
